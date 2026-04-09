1b6b92498e940ca74785b04fe379fb2f9c78c993
chaudhariamanullah <chaudhari.amanullah0@gmail.com>
2026-04-10 00:26:38 +0530

    Change SSL rejectUnauthorized to false

--- src/config/db.ts
+++ src/config/db.ts
@@ -13,7 +13,7 @@ const pool: Pool = mysql.createPool({
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_NAME!,
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     }
 });
 
